8431a361bd0358a3170f53df5ee3655e84ba53e9
Althafkv <althafkvworks@gmail.com>
2023-10-09 15:05:59 +0530

    Updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-qsgaiirfgsqapmpa.us.auth0.com"
       clientId="WujCOYtNKTsB27RDMbynSFTWxUGP9IBL"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://sky-real-mern.vercel.app",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
